9359060809b78bd9b096db25532b30e0df67efd0
ChrisSidebotham-MSFT <48600046+ChrisSidebotham@users.noreply.github.com>
2023-11-27 10:39:18 +0000

    Refactor Find-GithubIssue function to improve code readability and remove unnecessary comments

--- utilities/pipelines/sharedScripts/Find-GitHubIssue.ps1
+++ utilities/pipelines/sharedScripts/Find-GitHubIssue.ps1
@@ -1,3 +1,33 @@
+# function Find-GithubIssue {
+#     [CmdletBinding()]
+#     param (
+#         [Parameter(Mandatory)]
+#         [string]$title
+#     )
+    
+#     $issueList = gh issue list --json number,title,assignees,labels,id | ConvertFrom-Json
+#     $issueDetails = ""
+    
+#     foreach ($issueItem in $issueList) {
+#         # $matchFound = $false
+#         Write-Verbose "trying to find $($title)"
+#         Write-Verbose "issueItem.title: $($issueItem.title)"
+#         if ($issueItem.title -eq $title) {
+#             # $matchFound = $true
+#             $issueDetails = "$($issueItem.number) | $($issueItem.Title) | $($issueItem.assignees.login)"
+#             Write-Output "Good News! Issue Found:: $($issueItem.number) | $($issueItem.title) | $($issueItem.assignees.login)"
+#             break
+#             # return $issueDetails
+#         }
+#         else {
+#             # Write-Output "No match found for: $($title)"
+#             return $issueDetails            
+#         }
+#     }
+# }
+
+
+
 function Find-GithubIssue {
     [CmdletBinding()]
     param (
@@ -5,21 +35,24 @@ function Find-GithubIssue {
         [string]$title
     )
     
-    $issues = gh issue list --json number,title,assignees,labels,id | ConvertFrom-Json
+    $issueList = gh issue list --json number,title,assignees,labels,id | ConvertFrom-Json
     $issueDetails = ""
-    
-    foreach ($issue in $issues) {
-        $matchFound = $false
-        if ($issue.title -eq $title) {
-            $matchFound = $true
-            $issueDetails = "$($issue.number) | $($issue.Title) | $($issue.assignees.login)"
-            Write-Output "Good News! Issue Found:: $($issue.number) | $($issue.title) | $($issue.assignees.login)"
-            break
-            # return $issueDetails
-        }
-        else {
-            # Write-Output "No match found for: $($title)"
-            return $issueDetails            
+
+    foreach ($issueItem in $issueList) {
+        Write-Verbose "trying to find $($title)"
+        Write-Verbose "issueItem.title: $($issueItem.title)"
+        if ($issueItem.title -eq $title) {
+            $issueDetails = "$($issueItem.number) | $($issueItem.Title) | $($issueItem.assignees.login)"
+            Write-Output "Good News! Issue Found:: $($issueDetails)"
+            return $issueDetails
         }
     }
+
+    # If no match is found in the loop, return an empty string
+    Write-Output "No match found for: $($title)"
+    return $issueDetails
 }
+
+# Call the function with a title
+# $result = Find-GithubIssue -title "Your Issue Title Here"
+# Write-Output $result
