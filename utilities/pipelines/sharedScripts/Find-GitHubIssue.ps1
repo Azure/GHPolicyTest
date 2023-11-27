cfe37ed1fa3fdbc620a509647480de4c10275bbc
ChrisSidebotham-MSFT <48600046+ChrisSidebotham@users.noreply.github.com>
2023-11-27 10:40:42 +0000

    Remove commented out code in Find-GithubIssue function

--- utilities/pipelines/sharedScripts/Find-GitHubIssue.ps1
+++ utilities/pipelines/sharedScripts/Find-GitHubIssue.ps1
@@ -1,33 +1,3 @@
-# function Find-GithubIssue {
-#     [CmdletBinding()]
-#     param (
-#         [Parameter(Mandatory)]
-#         [string]$title
-#     )
-    
-#     $issueList = gh issue list --json number,title,assignees,labels,id | ConvertFrom-Json
-#     $issueDetails = ""
-    
-#     foreach ($issueItem in $issueList) {
-#         # $matchFound = $false
-#         Write-Verbose "trying to find $($title)"
-#         Write-Verbose "issueItem.title: $($issueItem.title)"
-#         if ($issueItem.title -eq $title) {
-#             # $matchFound = $true
-#             $issueDetails = "$($issueItem.number) | $($issueItem.Title) | $($issueItem.assignees.login)"
-#             Write-Output "Good News! Issue Found:: $($issueItem.number) | $($issueItem.title) | $($issueItem.assignees.login)"
-#             break
-#             # return $issueDetails
-#         }
-#         else {
-#             # Write-Output "No match found for: $($title)"
-#             return $issueDetails            
-#         }
-#     }
-# }
-
-
-
 function Find-GithubIssue {
     [CmdletBinding()]
     param (
@@ -52,7 +22,3 @@ function Find-GithubIssue {
     Write-Output "No match found for: $($title)"
     return $issueDetails
 }
-
-# Call the function with a title
-# $result = Find-GithubIssue -title "Your Issue Title Here"
-# Write-Output $result
