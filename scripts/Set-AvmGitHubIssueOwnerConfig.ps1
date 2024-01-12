011afb5599e0cd0231ed1d9b116a25341e7760b9
Rainer Halanek <rahalan@microsoft.com>
2024-01-12 13:51:24 +0100

    remove filter

--- scripts/Set-AvmGitHubIssueOwnerConfig.ps1
+++ scripts/Set-AvmGitHubIssueOwnerConfig.ps1
@@ -48,7 +48,7 @@ Function Get-AvmCsvData {
   # Convert the CSV content to a PowerShell object
   $formattedBicepFullCsv = ConvertFrom-CSV $unfilteredCSV.Content
   # Filter the CSV data where the ModuleStatus is 'Module Available :green_circle:'
-  $filterCsvAvailableBicepModule = $formattedBicepFullCsv | Where-Object { $_.ModuleStatus -eq 'Module Available :green_circle:' }
+  $filterCsvAvailableBicepModule = $formattedBicepFullCsv # | Where-Object { $_.ModuleStatus -eq 'Module Available :green_circle:' }
 
   # Loop through each item in the filtered data
   foreach ($item in $filterCsvAvailableBicepModule) {
