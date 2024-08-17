f6f3ccb209e737fc5b3a7a708566f1a6f6e9eaaf
Mate Barabas <barabasmate@gmail.com>
2024-08-16 18:45:38 -0700

    test

---
+++ utilities/tools/module-indexes/Test-AvmModuleIndexCsv.ps1
@@ -0,0 +1,67 @@
+<#
+.SYNOPSIS
+    This script tests the Azure Verified Modules (AVM) module index CSV file for correctness and completeness.
+
+.DESCRIPTION
+    The Test-AvmModuleIndexCsv.ps1 script is designed to validate the structure and content of the AVM module index CSV file.
+    It checks for required columns, validates data formats, and ensures there are no missing or duplicate entries.
+
+.PARAMETER CsvFilePath
+    The path to the CSV file that needs to be tested.
+
+.PARAMETER IacLanguage
+    The Infrastructure as Code (IaC) language for which the module index is being tested (Bicep or Terraform).
+
+.EXAMPLE
+    .\Test-AvmModuleIndexCsv.ps1 -CsvFilePath "C:\path\to\module-index.csv" -IacLanguage "Bicep"
+    This command runs the script on the specified CSV file for the Bicep language and writes the results to the specified log file.
+
+.EXAMPLE
+    .\Test-AvmModuleIndexCsv.ps1 -CsvFilePath "C:\path\to\module-index.csv" -IacLanguage "Terraform"
+    This command runs the script on the specified CSV file for the Terraform language and writes the results to the default log file.
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Enter the full path to the CSV file.")]
+    [ValidateNotNullOrEmpty()]
+    [Alias("Path")]
+    [array]$CsvFiles = @(
+      $(Join-Path (Get-Item -Path $PSScriptRoot).parent.parent.parent.parent.FullName "docs" "static" "module-indexes" "BicepResourceModules.csv"),
+      $(Join-Path (Get-Item -Path $PSScriptRoot).parent.parent.parent.parent.FullName "docs" "static" "module-indexes" "BicepPatternModules.csv"),
+      $(Join-Path (Get-Item -Path $PSScriptRoot).parent.parent.parent.parent.FullName "docs" "static" "module-indexes" "BicepUtilityModules.csv"),
+      $(Join-Path (Get-Item -Path $PSScriptRoot).parent.parent.parent.parent.FullName "docs" "static" "module-indexes" "TerraformResourceModules.csv"),
+      $(Join-Path (Get-Item -Path $PSScriptRoot).parent.parent.parent.parent.FullName "docs" "static" "module-indexes" "TerraformPatternModules.csv"),
+      $(Join-Path (Get-Item -Path $PSScriptRoot).parent.parent.parent.parent.FullName "docs" "static" "module-indexes" "TerraformUtilityModules.csv")
+    )
+)
+
+# $RepoRoot = (Get-Item -Path $PSScriptRoot).parent.parent.parent.parent.FullName
+
+$testFile = Join-Path $RepoRoot "utilities" "tools" "module-indexes" "module-index.tests.ps1"
+
+
+# $csvFiles =  @(
+#   $(Join-Path $RepoRoot "docs" "static" "module-indexes" "BicepResourceModules.csv"),
+#   $(Join-Path $RepoRoot "docs" "static" "module-indexes" "BicepPatternModules.csv"),
+#   $(Join-Path $RepoRoot "docs" "static" "module-indexes" "BicepUtilityModules.csv"),
+#   $(Join-Path $RepoRoot "docs" "static" "module-indexes" "TerraformResourceModules.csv"),
+#   $(Join-Path $RepoRoot "docs" "static" "module-indexes" "TerraformPatternModules.csv"),
+#   $(Join-Path $RepoRoot "docs" "static" "module-indexes" "TerraformUtilityModules.csv")
+# )
+
+
+foreach ($file in $csvFiles) {
+  $pesterConfiguration = @{
+    Run    = @{
+      Container = New-PesterContainer -Path $testFile -Data @{
+        CsvFilePath = $file
+      }
+      PassThru  = $false
+    }
+    Output = @{
+      Verbosity = 'Detailed'
+    }
+  }
+  Invoke-Pester -Configuration $pesterConfiguration
+}
